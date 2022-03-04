45cf9f8685484810b035d8519e82b11668abf794
BramVerb <33070319+BramVerb@users.noreply.github.com>
2022-03-04 10:51:27 +0100

    Gpu restructure

--- web/gpu.js
+++ web/gpu.js
@@ -17,7 +17,8 @@ function loadShader(gl, type, source, name) {
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
     console.error(
-      `An error occurred compiling the shaders: ${name}\n` + gl.getShaderInfoLog(shader)
+      `An error occurred compiling the shaders: ${name}\n` +
+        gl.getShaderInfoLog(shader)
     );
     gl.deleteShader(shader);
     return null;
@@ -28,7 +29,12 @@ function loadShader(gl, type, source, name) {
 
 function initShaderProgram(gl, vsSource, fsSource) {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource, "VERTEX");
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource, "FRAGMENT");
+  const fragmentShader = loadShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fsSource,
+    "FRAGMENT"
+  );
 
   // Create the shader program
 
@@ -58,7 +64,6 @@ var getSourceSynch = function (url) {
 };
 
 function initBuffers(gl) {
-
   // Create a buffer for the square's positions.
 
   const positionBuffer = gl.createBuffer();
@@ -70,20 +75,13 @@ function initBuffers(gl) {
 
   // Now create an array of positions for the square.
 
-  const positions = [
-     1.0,  1.0,
-    -1.0,  1.0,
-     1.0, -1.0,
-    -1.0, -1.0,
-  ];
+  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
 
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
 
-  gl.bufferData(gl.ARRAY_BUFFER,
-                new Float32Array(positions),
-                gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
   return {
     position: positionBuffer,
@@ -91,10 +89,10 @@ function initBuffers(gl) {
 }
 
 function drawScene(gl, programInfo, buffers) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-  gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
+  gl.clearDepth(1.0); // Clear everything
+  gl.enable(gl.DEPTH_TEST); // Enable depth testing
+  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
   // Clear the canvas before we start drawing on it.
 
@@ -103,22 +101,22 @@ function drawScene(gl, programInfo, buffers) {
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   {
-    const numComponents = 2;  // pull out 2 values per iteration
-    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-    const normalize = false;  // don't normalize
-    const stride = 0;         // how many bytes to get from one set of values to the next
-                              // 0 = use type and numComponents above
-    const offset = 0;         // how many bytes inside the buffer to start from
+    const numComponents = 2; // pull out 2 values per iteration
+    const type = gl.FLOAT; // the data in the buffer is 32bit floats
+    const normalize = false; // don't normalize
+    const stride = 0; // how many bytes to get from one set of values to the next
+    // 0 = use type and numComponents above
+    const offset = 0; // how many bytes inside the buffer to start from
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexPosition);
+      programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
 
   // Tell WebGL to use our program when drawing
@@ -139,36 +137,55 @@ function drawScene(gl, programInfo, buffers) {
   }
 }
 
-function gpu() {
-  const canvas = document.getElementById("canvas");
-  const gl = canvas.getContext("webgl2");
-  if (gl == null) {
-    alert("unable to init opengl");
-    return;
-  }
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  console.log(gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
 
-  const vshader = getSourceSynch("vshader.vs");
-  const fshader = getSourceSynch("fshader.fs");
-  const shaderProgram = initShaderProgram(gl, vshader, fshader);
-  const programInfo = {
-    program: shaderProgram,
-    attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, "vertexPosition"),
-    },
-    uniformLocations: {
-      // projectionMatrix: gl.getUniformLocation(
-      //   shaderProgram,
-      //   "uProjectionMatrix"
-      // ),
-      // modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-    },
-  };
-  const quad = initBuffers(gl)
-  drawScene(gl, programInfo, quad);
+function frame() {
+}
+
+class Renderer {
+  constructor() {
+    const canvas = document.getElementById("canvas");
+    this.gl = canvas.getContext("webgl2");
+    if (gl == null) {
+      alert("unable to init opengl");
+      return;
+    }
+    // Set clear color to black, fully opaque
+    this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    this.gl.clear(gl.COLOR_BUFFER_BIT);
+    console.log(this.gl.getParameter(this.gl.SHADING_LANGUAGE_VERSION));
+
+    const vshader = getSourceSynch("vshader.vs");
+    const fshader = getSourceSynch("fshader.fs");
+    this.shaderProgram = initShaderProgram(this.gl, vshader, fshader);
+    this.programInfo = {
+      program: shaderProgram,
+      attribLocations: {
+        vertexPosition: this.gl.getAttribLocation(this.shaderProgram, "vertexPosition"),
+      },
+      uniformLocations: {
+        // projectionMatrix: gl.getUniformLocation(
+        //   shaderProgram,
+        //   "uProjectionMatrix"
+        // ),
+        // modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+      },
+    };
+    this.quad = initBuffers(this.gl);
+  }
+
+  setupScene(){
+  }
+
+  frame() {
+    setupScene();
+    drawScene(this.gl, this.programInfo, this.quad);
+    // requestAnimationFrame(this.frame.bind(this));
+  }
+}
+
+function gpu() {
+  const renderer = new Renderer();
+  renderer.frame();
 }
 // gpu();
